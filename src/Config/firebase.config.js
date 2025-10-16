d24e5d017b57272eb22af85f32884c480fa6768f
AtikHasan16 <akhasannaeem@gmail.com>
2025-10-17 04:23:18 +0600

    added environt variable

--- src/Config/firebase.config.js
+++ src/Config/firebase.config.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import getAuth from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBNpMh42EaRD_RuZV7p3rfoTAN7bf8p6I",
-  authDomain: "private-authentication-page.firebaseapp.com",
-  projectId: "private-authentication-page",
-  storageBucket: "private-authentication-page.firebasestorage.app",
-  messagingSenderId: "346402232327",
-  appId: "1:346402232327:web:39fc43b34554050a3df2a2",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
