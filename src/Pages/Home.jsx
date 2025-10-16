5fadb5c889da13072965e78511dcbc2bb73e33ec
AtikHasan16 <akhasannaeem@gmail.com>
2025-10-17 04:40:13 +0600

    added some fix

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
           <div className=" flex flex-col  gap-y-16">
             <div>
               <div className="  text-center">
-                <h1 className="text-slate-900 md:text-5xl text-4xl font-bold !leading-tight">
-                  Simple <span className="text-sky-800">Authentication</span>{" "}
+                <h1 className="text-slate-900 md:text-6xl text-4xl font-bold">
+                  Simple <span className="text-sky-800">Authentication</span>
                   Page With Minimal Design
                 </h1>
                 <p className="text-slate-600 text-base leading-relaxed mt-6">
